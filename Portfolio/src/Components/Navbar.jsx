d041189b122f764d3e39612d9c7a991d053b4256
unknown <jatinjangra514@gmail.com>
2025-07-15 11:56:21 +0530

    initial commit

--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -1,8 +1,12 @@
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaBars } from "react-icons/fa";
 import { FaBuildingColumns } from "react-icons/fa6";
 import { PiPhoneCall } from "react-icons/pi";
 import { MdMiscellaneousServices } from "react-icons/md";
 import { IoIosHelpCircle } from "react-icons/io";
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+
+
 
 
 export default function Navbar() {
@@ -12,14 +16,16 @@ export default function Navbar() {
         { name: "Contact Us", icon: <PiPhoneCall size={19} />, link: "/contact_us" },
         { name: "Services", icon: <MdMiscellaneousServices size={18} />, link: "/services" },
         { name: "Help", icon: <IoIosHelpCircle size={18} />, link: "/help" },
-    ]
+    ];
+
+    const [open, Setopen] = useState(true)
 
     return (
-        <nav className="flex items-center justify-between bg-gradient-to-r from-slate-900 to-gray-700 py-4 px-8">
+        <nav className="flex items-center justify-between bg-gradient-to-r from-slate-900 to-gray-700 py-5 px-8">
             <a href="/"><h1 className="text-white text-2xl font-bold hover:scale-125 duration-200"><span className="text-red-500 text-3xl font-extrabold">J</span>atin</h1></a>
 
             {/*navlinks*/}
-            <ul className="flex gap-10 items-center">
+            <ul className="lg:flex gap-10 items-center hidden">
                 {MENUDATA.map(({ name, icon, link }, k) => (
                     <li key={k}>
                         <a href={link} className="flex gap-2 items-center text-white hover:text-gray-400 transition duration-200" >{icon}
@@ -30,9 +36,37 @@ export default function Navbar() {
             </ul>
 
             {/*buttons signup and signin*/}
-            <div className="flex gap-3 items-center">
+            <div className="lg:flex gap-3 items-center hidden">
                 <button className="px-4 py-2 bg-white rounded-lg hover:bg-gray-400 transition duration-200 hover:translate-y-[-3px] active:scale-[0.9]">Sign In</button>
                 <button className="px-4 py-2 bg-black rounded-lg text-white hover:bg-gray-800 transition duration-200 hover:translate-y-[-3px] active:scale-[0.9]">Sign Up</button>
+            </div>
+
+            {/* mobile view  */}
+
+            <div onClick={() => Setopen(!open)} className="text-3xl text-white lg:hidden">
+                {
+                    open ? <FaBars className="hover:text-black" /> : <RxCross2 className="hover:text-black" />
+                }
+                {
+                    !open &&
+                    <div className="bg-gradient-to-r from-slate-900 to-gray-700 absolute w-full top-19 left-0 flex flex-col">
+                        <ul className="flex flex-col gap-5 items-center text-xl border-black rounded-2xl py-2 mb-3 mx-auto w-[90%] bg-gradient-to-r from-slate-700 to-gray-900">
+                            {MENUDATA.map(({ name, icon, link }, k) => (
+                                <li key={k}>
+                                    <a href={link} className="flex gap-2 items-center text-white hover:text-gray-400 transition duration-200" >{icon}
+                                        <span>{name}</span>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+
+                        {/*buttons signup and signin*/}
+                        <div className="flex gap-3 items-center flex-col mb-3">
+                            <button className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-400 transition duration-200 hover:translate-y-[-3px] active:scale-[0.9] w-[90%]">Sign In</button>
+                            <button className="px-4 py-2 bg-black rounded-lg text-white hover:bg-gray-800 transition duration-200 hover:translate-y-[-3px] active:scale-[0.9] w-[90%]">Sign Up</button>
+                        </div>
+                    </div>
+                }
             </div>
 
         </nav>
